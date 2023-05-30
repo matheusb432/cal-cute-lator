6461953f16f3978cca484f5941e497545c679ec7
Matheus Bastos <matheus.mab432@gmail.com>
2023-05-30 18:50:05 -0300

    chore: initial PWA config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
 	import './styles.scss';
+	import { onMount } from 'svelte';
+	import { pwaInfo } from 'virtual:pwa-info';
+
+	onMount(async () => {
+		if (pwaInfo) {
+			const { registerSW } = await import('virtual:pwa-register' as any);
+			registerSW({
+				immediate: true,
+				onRegistered(r: any) {
+					console.log(`SW Registered: ${r}`);
+				},
+				onRegisterError(error: any) {
+					console.log('SW registration error', error);
+				}
+			});
+		}
+	});
+
+	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
+
+<svelte:head>
+	{@html webManifest}
+</svelte:head>
 
 <main>
 	<slot />
